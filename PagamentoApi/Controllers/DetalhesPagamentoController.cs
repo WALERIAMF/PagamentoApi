3e1f47efbede70aee7e1a816f6c98eafaee78881
waleriamf <waleriamf@gmail.com>
2021-07-08 10:17:20 -0300

    primeira versao com banco

--- PagamentoApi/Controllers/DetalhesPagamentoController.cs
+++ PagamentoApi/Controllers/DetalhesPagamentoController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PagamentoApi.Controllers
 {
@@ -11,5 +12,36 @@ namespace PagamentoApi.Controllers
     [ApiController]
     public class DetalhesPagamentoController : ControllerBase
     {
+        // GET: api/<DetalhesPagamentoController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DetalhesPagamentoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DetalhesPagamentoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DetalhesPagamentoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DetalhesPagamentoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
