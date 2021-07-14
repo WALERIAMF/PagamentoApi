598a4350e7dd783a3cd087a2e7735b6f73c8d6c9
waleriamf <waleriamf@gmail.com>
2021-07-14 14:05:38 -0300

    CRUD

--- PagamentoApi/Controllers/DetalhesPagamentoController.cs
+++ PagamentoApi/Controllers/DetalhesPagamentoController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
+using Microsoft.EntityFrameworkCore;
+using PagamentoData.Context;
+using PagamentoData.Repository;
+using PagamentoDomain.IRepository;
+using PagamentoDomain.Model;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +17,84 @@ namespace PagamentoApi.Controllers
     [ApiController]
     public class DetalhesPagamentoController : ControllerBase
     {
-        // GET: api/<DetalhesPagamentoController>
+        private readonly Context _context;
+
+        public DetalhesPagamentoController(Context context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<DetalhesPagamentoModel>>> GetDetalhesPagamento()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.DetalhesPagamento.ToListAsync();
         }
 
-        // GET api/<DetalhesPagamentoController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<DetalhesPagamentoModel>> GetDetalhesPagamento(int id)
         {
-            return "value";
+            var detalhesPagamento = await _context.DetalhesPagamento.FindAsync(id);
+            if(detalhesPagamento == null)
+            {
+                return NotFound();
+            }
+            return detalhesPagamento;
         }
 
-        // POST api/<DetalhesPagamentoController>
+        [HttpPut("(id")]
+        public async Task<IActionResult> PutDetalhesPagamento(string id, DetalhesPagamentoModel detalhesPagamento)
+        {
+            if (id != detalhesPagamento.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(detalhesPagamento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DetalhesPagamentoExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+       
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<DetalhesPagamentoModel>> PostDetalhesPagamento(DetalhesPagamentoModel detalhesPagamento)
         {
+            _context.DetalhesPagamento.Add(detalhesPagamento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDetalhesPagamento", new {id = detalhesPagamento.Id}, detalhesPagamento);
         }
 
-        // PUT api/<DetalhesPagamentoController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<DetalhesPagamentoController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteDetalhesPagamento(string id)
         {
+            var detalhesPagamento = await _context.DetalhesPagamento.FindAsync(id);
+            if(detalhesPagamento == null)
+            {
+                return NotFound();
+            }
+
+            _context.DetalhesPagamento.Remove(detalhesPagamento);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DetalhesPagamentoExist(string id)
+        {
+            return _context.DetalhesPagamento.Any(e => e.Id == id);
         }
     }
 }
--- PagamentoDomain/IServiceBus/IServiceBusDetalhesPagamento.cs
+++ PagamentoDomain/IServiceBus/IServiceBusDetalhesPagamento.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PagamentoDomain.IServiceBus
+﻿namespace PagamentoDomain.IServiceBus
 {
-    class IServiceBusDetalhesPagamento
+    public interface IServiceBusDetalhesPagamento
     {
+        void Subscribe();
     }
 }
